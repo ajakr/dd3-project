d3e9a534bd4c312d15e5471ec085283464be53e4
ajakr <jake.yates2004@gmail.com>
2023-01-13 18:26:35 -0500

    maybe working spawn

--- DD3 - please/Assets/spawn.cs
+++ DD3 - please/Assets/spawn.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class spawn : MonoBehaviour
 {
+  private GameObject spawnerobj;
+  private Vector3 spawnerV3;
     // Start is called before the first frame update
   void Awake()
   {
-      gameObject.transform.Translate(0, 15, 0);
+      spawnerobj = GameObject.Find("spawner");
+      spawnerV3 = spawnerobj.transform.position;
+      gameObject.transform.Translate(spawnerV3);
   }
 }
