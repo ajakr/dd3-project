3e1e5923dca11031f4385d0560787326460d483c
ajakr <jake.yates2004@gmail.com>
2023-01-23 10:29:13 -0600

    bugs

--- DD3 - please/Assets/Jake folder/CustomFOV.cs
+++ DD3 - please/Assets/Jake folder/CustomFOV.cs
@@ -4,55 +4,28 @@ using UnityEngine;
 
 public class CustomFOV : MonoBehaviour
 {
-    public float viewRange = 12f;
     public LayerMask enemies;
-    public float timeBetweenAtacks;
-    bool alreadyAttacked;
     public float attackRange;
     public bool playerinattack;
     public GameObject currentEnemy;
-    public bool checkedEnemy;
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        StartCoroutine("cannoncheck");
+        playerinattack = Physics.CheckSphere(transform.position, attackRange, enemies);
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, attackRange, enemies);
+        foreach(Collider hitCollider in hitColliders)
+        {
+            currentEnemy = hitCollider.GetComponent<Collider>().transform.parent.gameObject;
+        }
     }
-
-
-    public IEnumerator cannoncheck()
+    
+    private void Start()
+    {
+        StartCoroutine("gct");
+    }
+    private IEnumerator gct()
     {
         WaitForSeconds wait = new WaitForSeconds(0.2f);
+        
         yield return wait;
-        playerinattack = Physics.CheckSphere(transform.position, attackRange, enemies);
     }
-
-    public void Update()
-    {
-        
-        while(playerinattack == true)
-        {
-            if(checkedEnemy == false)
-            {
-                checkedEnemy = true;
-                CheckEnemy();
-            }
-        }
-        
-    }
-    void CheckEnemy()
-    {
-        Debug.Log("ahesahoogah");
-        Collider[] enemyhitcollider = Physics.OverlapSphere(transform.position, attackRange, enemies);
-        for(int i = 0; i<=enemyhitcollider.Length; i++)
-        {
-            currentEnemy = enemyhitcollider[i].gameObject;
-            break;
-        }
-    }
-    void AttackEnemy()
-    {
-        StopCoroutine("CheckingForEnemy");
-        transform.LookAt(currentEnemy.transform);
-    }
-
 }
