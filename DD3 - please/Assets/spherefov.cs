7365c387be8902a25641b8d1b79f7e0a9e91b5ef
ajakr <jake.yates2004@gmail.com>
2023-01-24 22:37:28 -0500

    new vision code

--- DD3 - please/Assets/spherefov.cs
+++ DD3 - please/Assets/spherefov.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class spherefov : MonoBehaviour
 {
+    public GameObject CurrentEnemy;
+    public bool enemyInRange;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +15,24 @@ public class spherefov : MonoBehaviour
     // Update is called once per frame
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other.gameObject);
+        if(other.tag == "Enemy")
+        {
+            CurrentEnemy = other.gameObject;
+            StartCoroutine("Attack");
+        }
     }    
-    
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Enemy")
+        {
+            StopCoroutine("Attack");
+            Destroy(CurrentEnemy);
+        }
+    }
+    private IEnumerator Attack()
+    {
+        yield return new WaitForSeconds(1);
+        Debug.Log("BAM");
+    }
+
 }
